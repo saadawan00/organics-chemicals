d82552753817adf96bc3f8dd19bc15f4d37d8904
Awais Rafiq <awaisrafeek@gmail.com>
2023-10-14 02:04:57 +0500

    Implement chemicals page products.

--- src/components/Pages/LabScientific/Chemicals.jsx
+++ src/components/Pages/LabScientific/Chemicals.jsx
@@ -9,6 +9,19 @@ var bnrimg = require("./../../../images/banner/2.jpg");
 
 export class Chemicals extends React.Component {
   render() {
+    const picturesData = [
+      { label: "Inorganic Reagents", imageName: "inorganic-chemistry" },
+      { label: "Acid & Base", imageName: "acidbase" },
+      { label: "Amino Acids & Resins", imageName: "aminoacids" },
+      { label: "Bulk Chemicals", imageName: "bulkchemicals" },
+      { label: "High Purity Solvents", imageName: "hps" },
+      { label: "Peroxides", imageName: "peroxides" },
+      { label: "Buffer & Volumatric Solutions", imageName: "buffervolumatric" },
+      { label: "Organic Reagents", imageName: "organicreagents" },
+      { label: "Titrating Agents", imageName: "titratingagents" },
+      { label: "Reference Standards", imageName: "refstandards" },
+    ];
+
     return (
       <>
         <Header2 />
@@ -27,7 +40,23 @@ export class Chemicals extends React.Component {
                   <div className="row">
                     <Sidebar navBarItems={sidebarItems} />
                     <div className="col-lg-8 col-md-12">
-                    {/* TODO: ADD PICTURES OF PRODUCTS */}
+                      <div className="section-content">
+                        {/* TITLE START */}
+                        <div className="text-left">
+                          <h2 className="wt-title">Chemicals</h2>
+                        </div>
+                        <div className="row">
+                          {picturesData.map(({label, imageName}) => (
+                            <div key={imageName} className="col-lg-3 col-xs-3 p-l4 p-r-4">
+                              <div className="d-flex align-items-center flex-column">
+
+                                <img width={80} src={require(`./../../../images/lab-scientific/chemicals/${imageName}.png`)} />
+                                <p className="p-t20 text-center">{label}</p>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
