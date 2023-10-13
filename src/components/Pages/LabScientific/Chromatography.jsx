792f2c87e25f1c25c7d417bcd2b871b4f874f32d
Awais Rafiq <awaisrafeek@gmail.com>
2023-10-14 01:17:16 +0500

    Implement sidebar items in other pages.

--- src/components/Pages/LabScientific/Chromatography.jsx
+++ src/components/Pages/LabScientific/Chromatography.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import Header2 from "../../Parts/Header2";
+import Footer from "../../Parts/Footer";
+import Banner from "./../../Segments/Banner";
+import Sidebar from "./../../Segments/Sidebar";
+import { sidebarItems } from '.';
 
-export const Chromatography = () => {
-  return (
-    <div>
-      Chromatography
-    </div>      
-  )
-};
+var bnrimg = require("./../../../images/banner/2.jpg");
+
+export class Chromatography extends React.Component {
+  render() {
+    return (
+      <>
+        <Header2 />
+        <div className="page-content">
+          {/* INNER PAGE BANNER */}
+          <Banner
+            title="Chromatography"
+            pagename="Chromatography"
+            bgimage={bnrimg.default}
+          />
+          {/* INNER PAGE BANNER END */}
+          <>
+            <div className="section-full small-device p-t80 p-b50 bg-white">
+              <div className="section-content">
+                <div className="container">
+                  <div className="row">
+                    <Sidebar navBarItems={sidebarItems} />
+                    <div className="col-lg-8 col-md-12">
+                    {/* TODO: ADD PICTURES OF PRODUCTS */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        </div>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Chromatography;
--- src/components/Pages/LabScientific/Glassware.jsx
+++ src/components/Pages/LabScientific/Glassware.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import Header2 from "../../Parts/Header2";
+import Footer from "../../Parts/Footer";
+import Banner from "./../../Segments/Banner";
+import Sidebar from "./../../Segments/Sidebar";
+import { sidebarItems } from '.';
 
-export const Glassware = () => {
-  return (
-    <div>
-      Glassware
-    </div>      
-  )
-};
+var bnrimg = require("./../../../images/banner/2.jpg");
+
+export class Glassware extends React.Component {
+  render() {
+    return (
+      <>
+        <Header2 />
+        <div className="page-content">
+          {/* INNER PAGE BANNER */}
+          <Banner
+            title="Glassware"
+            pagename="Glassware"
+            bgimage={bnrimg.default}
+          />
+          {/* INNER PAGE BANNER END */}
+          <>
+            <div className="section-full small-device p-t80 p-b50 bg-white">
+              <div className="section-content">
+                <div className="container">
+                  <div className="row">
+                    <Sidebar navBarItems={sidebarItems} />
+                    <div className="col-lg-8 col-md-12">
+                    {/* TODO: ADD PICTURES OF PRODUCTS */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        </div>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Glassware;
