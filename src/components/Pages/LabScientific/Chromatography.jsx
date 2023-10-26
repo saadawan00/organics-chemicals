3ad9f44e544ba5ded1ece65dee203a2b1de5464e
Awais <awaisrafeek@gmail.com>
2023-10-26 22:20:08 +0500

    Add chromatography page design.

--- src/components/Pages/LabScientific/Chromatography.jsx
+++ src/components/Pages/LabScientific/Chromatography.jsx
@@ -4,11 +4,39 @@ import Footer from "../../Parts/Footer";
 import Banner from "./../../Segments/Banner";
 import Sidebar from "./../../Segments/Sidebar";
 import { sidebarItems } from '.';
+import { NavLink } from "react-router-dom";
 
 var bnrimg = require("./../../../images/banner/2.jpg");
 
+const ChromatographyCard = ({ label, imageName }) => {
+  return (
+    <div key={imageName} className="col-lg-3 col-xs-3 p-60 p-r2 p-t10 p-b30">
+      <div className="wt-icon-card shadow d-flex align-items-center flex-column border p-b10 p-t10 p-r4 p-l4 hover-opacity-dim pointer hover-border-blue">
+        <img width={80} src={require(`./../../../images/lab-scientific/chromatography/${imageName}.jpg`)} alt="" />
+        <p className="p-t20 text-center" style={{ height: 50, fontSize: 14 }}>{label}</p>
+        <div style={{ position: "absolute", top: 80 }}>
+          <NavLink
+            to={"#"}
+            className="contact-slide-show get-quote-sm-btn m-t10 "
+            style={{ opacity: 0 }}
+          >
+            Get Quote
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  )
+};
+
 export class Chromatography extends React.Component {
   render() {
+    const picturesData = [
+      { label: "Chromatographic Columns", imageName: "Chromatographic Columns" },
+      { label: "Chromatographic Flash Reservoirs", imageName: "Chromatographic Flash Reservoirs" },
+      { label: "Fitting and Acessories", imageName: "Fitting and Acessories" },
+      { label: "Fitting and Acessories", imageName: "Fitting and Acessories" },
+      { label: "HPLC Columns", imageName: "HPLC Columns" },
+    ];
     return (
       <>
         <Header2 />
@@ -27,7 +55,19 @@ export class Chromatography extends React.Component {
                   <div className="row">
                     <Sidebar navBarItems={sidebarItems} />
                     <div className="col-lg-8 col-md-12">
-                    {/* TODO: ADD PICTURES OF PRODUCTS */}
+                      <div className="section-content">
+                        {/* TITLE START */}
+                        <div className="text-left">
+                          <h2 className="wt-title">Chromatography</h2>
+                        </div>
+                        <div className="container">
+                          <div className="row">
+                            {picturesData.map((item, idx) => (
+                              <ChromatographyCard key={idx} {...item} />
+                            ))}
+                        </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
