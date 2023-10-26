1eb0eb7d16846ea92145aa59ffcb2fac0fe70e50
Awais <awaisrafeek@gmail.com>
2023-10-26 22:27:11 +0500

    Chemicals page design

--- src/components/Pages/LabScientific/Chemicals.jsx
+++ src/components/Pages/LabScientific/Chemicals.jsx
@@ -1,11 +1,33 @@
+
 import React from "react";
 import Header2 from "../../Parts/Header2";
 import Footer from "../../Parts/Footer";
 import Banner from "./../../Segments/Banner";
 import Sidebar from "./../../Segments/Sidebar";
 import { sidebarItems } from '.';
+import { NavLink } from "react-router-dom";
 
 var bnrimg = require("./../../../images/banner/2.jpg");
+
+const ChemicalsCard = ({ label, imageName }) => {
+  return (
+    <div key={imageName} className="col-lg-3 col-xs-3 p-60 p-r2 p-t10 p-b30">
+      <div className="wt-icon-card shadow d-flex align-items-center flex-column border p-b10 p-t10 p-r4 p-l4 hover-opacity-dim pointer hover-border-blue">
+        <img width={80} src={require(`./../../../images/lab-scientific/chemicals/${imageName}.png`)} alt="" />
+        <p className="p-t20 text-center" style={{ height: 50, fontSize: 14 }}>{label}</p>
+        <div style={{ position: "absolute", top: 80 }}>
+          <NavLink
+            to={"#"}
+            className="contact-slide-show get-quote-sm-btn m-t10 "
+            style={{ opacity: 0 }}
+          >
+            Get Quote
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export class Chemicals extends React.Component {
   render() {
@@ -20,8 +42,8 @@ export class Chemicals extends React.Component {
       { label: "Organic Reagents", imageName: "organicreagents" },
       { label: "Titrating Agents", imageName: "titratingagents" },
       { label: "Reference Standards", imageName: "refstandards" },
-    ];
 
+    ];
     return (
       <>
         <Header2 />
@@ -45,15 +67,12 @@ export class Chemicals extends React.Component {
                         <div className="text-left">
                           <h2 className="wt-title">Chemicals</h2>
                         </div>
-                        <div className="row">
-                          {picturesData.map(({label, imageName}) => (
-                            <div key={imageName} className="col-lg-3 col-xs-3 p-l4 p-r4 p-t10 p-b10">
-                              <div className="d-flex align-items-center flex-column">
-                                <img width={80} src={require(`./../../../images/lab-scientific/chemicals/${imageName}.png`)} />
-                                <p className="p-t20 text-center">{label}</p>
-                              </div>
-                            </div>
-                          ))}
+                        <div className="container">
+                          <div className="row">
+                            {picturesData.map((item, idx) => (
+                              <ChemicalsCard key={idx} {...item} />
+                            ))}
+                        </div>
                         </div>
                       </div>
                     </div>
