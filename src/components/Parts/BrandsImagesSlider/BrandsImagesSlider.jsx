3a723f8c79564d9a5b041853357ef9d1aa86b6ac
Awais <awaisrafeek@gmail.com>
2023-10-29 03:07:02 +0500

    Replace images.

--- src/components/Parts/BrandsImagesSlider/BrandsImagesSlider.jsx
+++ src/components/Parts/BrandsImagesSlider/BrandsImagesSlider.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 
-import Picture1 from './../../../images/brands/Picture1.jpg'
-import Picture2 from './../../../images/brands/Picture2.jpg'
-import Picture3 from './../../../images/brands/Picture3.jpg'
-import Picture4 from './../../../images/brands/Picture4.jpg'
-import Picture5 from './../../../images/brands/Picture5.jpg'
+import Picture1 from './../../../images/brands/Picture1.png'
+import Picture2 from './../../../images/brands/Picture2.png'
+import Picture3 from './../../../images/brands/Picture3.png'
+import Picture4 from './../../../images/brands/Picture4.png'
+import Picture5 from './../../../images/brands/Picture5.png'
 import Picture6 from './../../../images/brands/Picture6.jpg'
-import Picture7 from './../../../images/brands/Picture7.jpg'
+import Picture7 from './../../../images/brands/Picture7.png'
 import Picture8 from './../../../images/brands/Picture8.jpg'
-import Picture9 from './../../../images/brands/Picture9.jpg'
-import Picture10 from './../../../images/brands/Picture10.jpg'
-import Picture11 from './../../../images/brands/Picture11.jpg'
-import Picture12 from './../../../images/brands/Picture12.jpg'
+import Picture9 from './../../../images/brands/Picture9.png'
+import Picture10 from './../../../images/brands/Picture10.png'
+import Picture11 from './../../../images/brands/Picture11.png'
+import Picture12 from './../../../images/brands/Picture12.png'
 import Picture13 from './../../../images/brands/Picture13.jpg'
-import Picture14 from './../../../images/brands/Picture14.jpg'
-import Picture15 from './../../../images/brands/Picture15.jpg'
-import Picture16 from './../../../images/brands/Picture16.jpg'
+import Picture14 from './../../../images/brands/Picture14.png'
+import Picture15 from './../../../images/brands/Picture15.png'
+import Picture16 from './../../../images/brands/Picture16.png'
+import Picture17 from './../../../images/brands/Picture17.png'
+import Picture18 from './../../../images/brands/Picture18.jpg'
+import Picture19 from './../../../images/brands/Picture19.png'
+import Picture20 from './../../../images/brands/Picture20.png'
+import Picture21 from './../../../images/brands/Picture21.png'
+import Picture22 from './../../../images/brands/Picture22.png'
+import Picture23 from './../../../images/brands/Picture23.png'
+import Picture24 from './../../../images/brands/Picture24.jpg'
+import Picture25 from './../../../images/brands/Picture25.png'
 
 import { ImagesSlider } from './../ImagesSlider/ImagesSlider';
 
@@ -37,6 +46,15 @@ export const BrandsImagesSlider = () => {
         Picture14,
         Picture15,
         Picture16,
+        Picture17,
+        Picture18,
+        Picture19,
+        Picture20,
+        Picture21,
+        Picture22,
+        Picture23,
+        Picture24,
+        Picture25,
       ].map((image) => ({
         id: crypto.randomUUID(),
         image
@@ -44,8 +62,25 @@ export const BrandsImagesSlider = () => {
 
     return (
         <div className="m-t30">
-          <p className="text-center font-22 m-b30">Large variety of stong brands in stock</p>
+          <div className="flex-center m-b30">
+            <p className="font-22 m-b0 m-r20">Large variety of stong brands</p>
+            <img width={50} src={require('./../../../images/in-stock.png')} alt="" />
+          </div>
           <ImagesSlider images={images} speed={25000} />
+          <div className="section-content bg-gray">
+            <div className="container">
+              <div className="row">
+                <div className="col-4 bg-gray p-t20 p-b20">
+                </div>
+                <div className="col-4 bg-gray p-t20 p-b20">
+
+                </div>
+                <div className="col-4 bg-gray p-t20 p-b20">
+
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
     );
 };
