478e77c8146890e60277c8b042c2e08d8af555fa
Wasif007 <wasifateeq007@hotmail.com>
2023-11-06 20:41:09 +0500

    footer added

--- src/components/Parts/Footer.jsx
+++ src/components/Parts/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Switcher from '../Segments/Switcher';
 
+import bestPriceIcon from './../../images/icons/best-price.png';
+import timelyDelieveryIcon from './../../images/icons/shipping-timed.png';
+import techSupportIcon from './../../images/icons/tech-support.png';
+
 var bgimage = require('./../../images/background/footer-bg.png');
 
 class Footer extends React.Component {
@@ -27,6 +31,32 @@ class Footer extends React.Component {
 
         return (
             <>
+             <div className="section-content bg-gray m-t30">
+        <div className="container">
+          <div className="row">
+            <div className="col-4 bg-gray p-t40 p-b40">
+              <div className="flex-center">
+                <img src={bestPriceIcon} width={25} className="m-r15 m-b5" /> 
+                <h4 className="m-0 text-center">
+                  Competitive Prices
+                </h4>
+              </div>
+            </div>
+            <div className="col-4 bg-gray p-t40 p-b40">
+              <div className="flex-center">
+                <img src={timelyDelieveryIcon} width={25} className="m-r15 m-b5" /> 
+                <h4 className="m-0 text-center">Timely Delivery</h4>
+              </div>
+            </div>
+            <div className="col-4 bg-gray p-t40 p-b40">
+              <div className="flex-center">
+                <img src={techSupportIcon} width={25} className="m-r15 m-b5" /> 
+                <h4 className="m-0 text-center">Free Technical Support</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
                 <footer className="site-footer footer-large footer-dark text-white footer-style1">
                     {/* FOOTER BLOCKES START */}
                     <div className="footer-top bg-no-repeat bg-bottom-right" style={{ backgroundImage: 'url('+bgimage.default+')' }}>
