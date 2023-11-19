70d6041a35c06256b409afc4a480b143f843297b
Awais <awaisrafeek@gmail.com>
2023-11-19 15:28:21 +0500

    revert sidebar

--- src/components/Segments/Sidebar.jsx
+++ src/components/Segments/Sidebar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import IndustrialInstrumentsPDF from './../../assets/media/pdf/Industrial Instruments Organics.pdf';
 
 class Sidebar extends React.Component {
   render() {
     const { navBarItems } = this.props;
 
     return (
-      <div className="col-lg-4 col-md-12 rightSidebar">
+      <>
         <div className="all_services">
           <ul>
             {navBarItems.map(({ label, route }, idx) => (
@@ -17,30 +16,7 @@ class Sidebar extends React.Component {
             ))}
           </ul>
         </div>
-        <div className="service-side-btn m-b30 site-bg-secondry p-a20 text-white">
-            <div className="wt-icon-box-wraper left">
-              <NavLink to={"#"} className="btn-block contact-slide-show p-a15 m-tb10">
-                <span className="text-black m-r10">
-                  <i className="fa fa-file-word-o" />
-                </span>
-                <strong className="text-uppercase text-black">
-                  {" "}
-                  Quick Inquiry
-                </strong>
-              </NavLink>
-            </div>
-            <div className="wt-icon-box-wraper left ">
-              <NavLink to={"#"} onClick={() => window.open(IndustrialInstrumentsPDF)} className="btn-block  p-a15 m-tb10">
-                <span className="text-black m-r10">
-                  <i className="fa fa-file-pdf-o" />
-                </span>
-                <strong className="text-uppercase text-black">
-                  Download .PDF
-                </strong>
-              </NavLink>
-            </div>
-          </div>
-      </div>
+      </>
     );
   }
 }
