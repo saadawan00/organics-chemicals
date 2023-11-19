8ecfaa963bdd9adf44de572cf86770a668eaa246
Awais <awaisrafeek@gmail.com>
2023-11-19 15:25:06 +0500

    Add industrial instruments doc

--- src/components/Segments/ServicesSidebar.jsx
+++ src/components/Segments/ServicesSidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import ListOfChemicalsPDF from './../../assets/media/pdf/List of Chemicals Organics.pdf';
 
 class ServicesSidebar extends React.Component {
   render() {
@@ -46,14 +47,20 @@ class ServicesSidebar extends React.Component {
               </li>
             </ul>
           </div>
-          {/* <div className="service-side-btn m-b30 site-bg-secondry p-a20 text-white">
-            <h2 className="wt-title">Brochures</h2>
-            <p>
-              View our 2020 financial prospectus brochure for an easy to read
-              guide on all of the services offer.
-            </p>
+          <div className="service-side-btn m-b30 site-bg-secondry p-a20 text-white">
+            <div className="wt-icon-box-wraper left">
+              <NavLink to={"#"} className="btn-block contact-slide-show p-a15 m-tb10">
+                <span className="text-black m-r10">
+                  <i className="fa fa-file-word-o" />
+                </span>
+                <strong className="text-uppercase text-black">
+                  {" "}
+                  Quick Inquiry
+                </strong>
+              </NavLink>
+            </div>
             <div className="wt-icon-box-wraper left ">
-              <NavLink to={"#"} className="btn-block  p-a15 m-tb10">
+              <NavLink to={"#"} onClick={() => window.open(ListOfChemicalsPDF)} className="btn-block  p-a15 m-tb10">
                 <span className="text-black m-r10">
                   <i className="fa fa-file-pdf-o" />
                 </span>
@@ -62,28 +69,7 @@ class ServicesSidebar extends React.Component {
                 </strong>
               </NavLink>
             </div>
-            <div className="wt-icon-box-wraper left">
-              <NavLink to={"#"} className="btn-block  p-a15 m-tb10">
-                <span className="text-black m-r10">
-                  <i className="fa fa-file-word-o" />
-                </span>
-                <strong className="text-uppercase text-black">
-                  {" "}
-                  Download .DOC
-                </strong>
-              </NavLink>
-            </div>
-            <div className="wt-icon-box-wraper left">
-              <NavLink to={"#"} className="btn-block p-a15 m-t10">
-                <span className="text-black m-r10">
-                  <i className="fa fa-file-powerpoint-o" />
-                </span>
-                <strong className="text-uppercase text-black">
-                  Download .PPT
-                </strong>
-              </NavLink>
-            </div>
-          </div> */}
+          </div>
         </div>
       </>
     );
